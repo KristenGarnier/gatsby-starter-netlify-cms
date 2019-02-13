6b131c88fbed42df7a13eff95fc9cc7a7b374c2a
kristengarnier <garnier.kristen@icloud.com>
2019-02-13 15:25:07 +0100

    udpate

--- src/templates/menu.js
+++ src/templates/menu.js
@@ -52,7 +52,7 @@ AboutPage.propTypes = {
 export default AboutPage
 
 export const aboutPageQuery = graphql`
-  query IndexPage($id: String!) {
+  query MenuPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
       html
       frontmatter {

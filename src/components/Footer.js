19a7df0c9e39353392166fb1c265e9d5a3fc20a6
kristengarnier <garnier.kristen@icloud.com>
2019-04-16 14:46:48 +0200

    Update footer with real data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,12 @@ const Footer = ({ children }) => (
     <StaticQuery
     query={graphql`
       query MenuBottomQuery {
-        allMarkdownRemark(filter: {frontmatter: {type:{eq: "bottom"} }}) {
-          edges {
-            node {
-              id,
-              frontmatter {
-                title,
-                description,
-                type,
-                link
+        markdownRemark(frontmatter: {templateKey: {eq: "index-page"}}) {
+          frontmatter {
+            partners {
+              link
+              logo {
+                publicURL
               }
             }
           }
@@ -21,18 +18,38 @@ const Footer = ({ children }) => (
       }
     `}
     render = {data => {
+
+      const partners = data.markdownRemark.frontmatter.partners.map((item, i) => <li key={i} className="footerPartnersListItem">
+        <a href="<!-- LINK -->">
+          <img src={item.logo.publicURL} alt="<!-- ALT -->" title="<!-- TITLE -->"/>
+        </a>
+      </li>);
+
       return (
-          <div>
-            {
-              data.allMarkdownRemark ?
-              data.allMarkdownRemark.edges.map((item, i) => {
-                return <Link className="navbar-item" to={item.node.frontmatter.link} key={i}>
-                  {item.node.frontmatter.title}
-                </Link>
-              }) :
-              ""
-            }
+        <footer className="footer">
+          <div className="container">
+            <div className="footerPartners grid">
+              <div className="gridItem gridItem--33">
+                <p className="footerLogo"><a href="<!-- LINK -->"><img src="/logo-white-footer.77e2e47c.svg" alt="<!-- ALT -->"/></a></p>
+              </div>
+              <div className="gridItem gridItem--66">
+                <h2>En partenariat avec</h2>
+                <ul className="footerPartnersList grid">
+                  {partners}
+                </ul>
+              </div>
+            </div>
+            <div className="footerLinks">
+              <ul className="footerLinksList">
+                <li className="footerLinksListItem"><a href="<!-- LINK -->">Mentions légales</a></li>
+                <li className="footerLinksListItem"><a href="<!-- LINK -->">Plan du site</a></li>
+              </ul>
+            </div>
+            <div className="footerCopyright">
+              <p>Designed with <i className="fas fa-heart"></i> by <a href="https://dribbble.com/SimonMazzega" target="_blank">Mazzega Simon</a> and coded with a lot of <i className="fas fa-mug-hot"></i> by <a href="https://www.nerdy-bear.com" target="_blank">Nerdy Bear</a></p>
+            </div>
           </div>
+        </footer>
       )
     }}
   />

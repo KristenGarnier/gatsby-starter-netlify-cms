f706386bc2c9d325e4379346423b9dc93006058b
kristengarnier <garnier.kristen@icloud.com>
2019-04-16 14:47:07 +0200

    Add font awesome and google font

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -36,6 +36,9 @@ const TemplateWrapper = ({ children }) => (
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/og-image.jpg" />
+
+          <link rel="stylesheet" async defer href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>>
+          <link href="https://fonts.googleapis.com/css?family=Heebo:300,400,700,800" rel="stylesheet" async defer></link>
         </Helmet>
         <Navbar />
         <div>{children}</div>
